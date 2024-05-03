0072213b195395cea9d661313bb931a74206dcf1
SimuhMamad <muhammadabdurrahman891@gmail.com>
2024-05-03 22:10:37 +0700

    vercel

--- topostgres.js
+++ topostgres.js
@@ -2,22 +2,22 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = process.env.PORT
+const port = 5000
 require('dotenv').config()
 
 const Pool = require('pg').Pool
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL ,
-})
 // const pool = new Pool({
-//   user: 'me',
-//   host: 'localhost',
-//   database: 'CommuFund',
-//   password: 'password',
-//   port: 5432,
+//   connectionString: process.env.POSTGRES_URL ,
 // })
+const pool = new Pool({
+  user: 'me',
+  host: 'localhost',
+  database: 'CommuFund',
+  password: 'password',
+  port: 5432,
+})
 
-app.use(express.json())
+// app.use(express.json())
 
 app.use(bodyParser.json())
 app.use(
@@ -26,18 +26,21 @@ app.use(
   })
 )
 
-pool.connect((err) => {
-  if (err) throw err
-  console.log("Connect to PostgreSQL sucsessfully")
-})
+// pool.connect((err) => {
+//   if (err) throw err
+//   console.log("Connect to PostgreSQL sucsessfully")
+// })
 
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/login.html');
-// });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/login.html');
+});
+// app.get('/', async(request, response) => {
+//   response.json({ info: 'Node.js, Express, and Postgres API' })
+// })
 
-app.get('/', async(request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+app.get('/switch', (req, res) => {
+  res.sendFile(__dirname + '/registrasi.html');
+});
 
 app.post('/postPass', async (request, response) => {
   try {
@@ -70,14 +73,15 @@ app.post('/postRegis', async (request, response) => {
     const email = request.body.email;
 
     
-    const queri = `INSERT INTO users (username, password, nama, no_hp, email) VALUE ('${username}', '${password}', '${name}', '${number}', '${email}')`
+    const queri = `INSERT INTO users (username, password, nama, no_hp, email) VALUES ('${username}', '${password}', '${name}', '${number}', '${email}')`
     console.log(queri)
     const result = await pool.query(queri);
     //console.log(result)
-    response.send("Registrasi berhasil")
+    response.send({msg: "Registrasi berhasil", text: "success"})
      // Ensure correct property name
   } catch (error) {
     console.error('Error fetching data from database:', error);
+    response.json({ text: 'failed' });
     response.status(500).send('Internal Server Error'); // Send appropriate error response
   }
 })
